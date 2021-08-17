57176cbe59e56e1b3bc0a8c4eea0bb6a017f2310
Knowmen <39981286+Shellyhackinglab@users.noreply.github.com>
2021-08-17 21:53:05 +0900

    Create pingpong_scoreboard.py

---
+++ pingpong_scoreboard.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+
+ALIGN = "left"
+FONT =("Courier",10,"normal")
+
+class Scoreboard1(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.speed(0)
+        self.score = 0
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(-260,-20)   
+
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}",align=ALIGN,font=FONT)  
+       # ↑self.bar.writeではない！
+       #前は ---> self.write(arg=f"Score: {self.score}",move=False,align="center",font=("Arial",18,"normal"))  
+       #のように書いていたけれど、ハードコーディングは好ましくないので、グローバル変数として外で定義しよう。
+        
+    def add_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+    
+
+class Scoreboard2(Scoreboard1):
+
+    def __init__(self):
+        super().__init__()
+        self.goto(-260,20)
+    
+    def update_scoreboard(self):
+        super().update_scoreboard()
+
+    def add_score(self):
+        pass   
